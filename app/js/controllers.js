c986a736d8cb3d6815bde4ea598a43fdf9430cf9
Richard Macias <RJNY86@gmail.com>
2014-10-23 13:00:01 -0400

    Initial commit, landing page and bootstrap
            Initial commit starting project, installing bootstrap and
            mocking up the landing page.

---
+++ app/js/controllers.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+  .controller('LandingPageController',[function () {
+
+  }])
+  .controller('MyCtrl1', [function() {
+
+  }])
+  .controller('MyCtrl2', [function() {
+
+  }]);
