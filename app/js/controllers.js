f26f315e2738d47b5b557c891ba442d690fc0803
Richard Macias <RJNY86@gmail.com>
2014-10-23 13:45:27 -0400

    Add logic to form for submitting to an array.
    	Added logic to the form field so that data will be saved
    	inside to browser to an array.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,11 @@ angular.module('myApp.controllers', [])
   .controller('LandingPageController',[function () {
 
   }])
-  .controller('WaitlistController',[function(){
-
+  .controller('WaitlistController',['$scope', function($scope){
+    $scope.parties = [];
+    $scope.party = {name:'',song:'', phone:''};
+    $scope.saveParty = function(){
+      $scope.parties.push($scope.party);
+      $scope.party = {name:'',song:'', phone:''};
+    }
   }])
