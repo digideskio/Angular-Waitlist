2308cd0115d107cc5e13a6b1ca5b075d43526220
Richard Macias <RJNY86@gmail.com>
2014-10-23 14:03:21 -0400

    Linked form to firebase database
    	Waitlist now writes to the firebase database.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,11 +6,15 @@ angular.module('myApp.controllers', [])
   .controller('LandingPageController',[function () {
 
   }])
-  .controller('WaitlistController',['$scope', function($scope){
-    $scope.parties = [];
+  .controller('WaitlistController',['$scope', '$firebase', function($scope,$firebase){
+    var partiesRef = new Firebase('https://kara-oke.firebaseio.com/')
+
+    $scope.parties = $firebase(partiesRef);
+
     $scope.party = {name:'',song:'', phone:''};
+
     $scope.saveParty = function(){
-      $scope.parties.push($scope.party);
+      $scope.parties.$add($scope.party);
       $scope.party = {name:'',song:'', phone:''};
-    }
-  }])
+    };
+  }]);
